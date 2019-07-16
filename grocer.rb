26fc9423ed8156f1456071fc22bf567cbbae8c9c
Cody Shaben <cshaben87@gmail.com>
2019-07-16 19:57:28 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,37 +1,3 @@
-
-
-def apply_coupons(cart, coupons)
-  coupons.each do |coupon|
-    if cart.keys.include? coupon[:item]
-      if cart[coupon[:item]][:count] >= coupon[:num]
-        new_name = "#{coupon[:item]} W/COUPON"
-        if cart[new_name]
-          cart[new_name][:count] += coupon[:num]
-        else
-          cart[new_name] = {
-            count: coupon[:num],
-            price: coupon[:cost]/coupon[:num],
-            clearance: cart[coupon[:item]][:clearance]
-          }
-        end
-        cart[coupon[:item]][:count] -= coupon[:num]
-      end
-    end
-  end
-  cart
-end
-
-def apply_clearance(cart)
-  cart.keys.each do |item|
-    if cart[item][:clearance]
-      cart[item][:price] = (cart[item][:price]*0.80).round(2)
-    end
-  end
-  cart
-end
-
-def checkout(cart, coupons)
-  consol_cart = consolidate_cart(cart)
   cart_with_coupons_applied = apply_coupons(consol_cart, coupons)
   cart_with_discounts_applied = apply_clearance(cart_with_coupons_applied)
 
@@ -41,4 +7,3 @@ def checkout(cart, coupons)
   end
   total > 100.00 ? (total * 0.90).round : total
 end
-
